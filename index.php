149d9d1a56f95bc1c19702f52c2a9d94bc5171f6
fabianlapiana <fabianlapiana@gmail.com>
2014-12-11 11:19:58 -0300

    Index

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+
+
+   $menu=$_GET['menu'];
+   
+?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns:fb="http://ogp.me/ns/fb#" xml:lang="es-es" lang="es-es"  >
+
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<script type="text/javascript" src="funciones.js"></script>
+
+
+
+
+
+<meta name="Language" content="es" />  
+<meta name="Copyright" content="All rights reserved." />  
+<meta name="Robots" content="index, follow, all" />  
+<meta http-equiv="Cache-Control" content="Public" />  
+<meta name="Distribution" content="Global" />  
+<meta name="revisit-after" content="1 days" />
+<meta name="google-site-verification" content="UnQDcogZ-U_3FPzVXH-PgsGnCjJZfjyDbVht_spC3vc" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+
+
+
+
+<title>SCADA</title>
+
+<link rel="StyleSheet" href="estilos.css" type="text/css">
+
+
+</head> 	
+
+<body >
+
+<div id="main" >
+	
+</div>
+
+
+
+
+
+</body>
+</html>
+		
